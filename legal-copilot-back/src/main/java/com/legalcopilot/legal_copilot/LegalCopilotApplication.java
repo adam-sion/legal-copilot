58b0274b529213270b31d6c3a38fbb4de1395407
Adam <adamsion74@gmail.com>
2025-05-14 21:20:21 +0300

    first commit

---
+++ legal-copilot-back/src/main/java/com/legalcopilot/legal_copilot/LegalCopilotApplication.java
@@ -0,0 +1,13 @@
+package com.legalcopilot.legal_copilot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LegalCopilotApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LegalCopilotApplication.class, args);
+	}
+
+}
---
+++ legal-copilot-back/src/test/java/com/legalcopilot/legal_copilot/LegalCopilotApplicationTests.java
@@ -0,0 +1,13 @@
+package com.legalcopilot.legal_copilot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LegalCopilotApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
