05406530fb2a0cfe0fecdac35596a8966d3a1057
Adam <adamsion74@gmail.com>
2025-05-20 08:26:40 +0300

    feat: ingestion service

--- legal-copilot-back/src/main/java/com/legalcopilot/legal_copilot/LegalCopilotApplication.java
+++ legal-copilot-back/src/main/java/com/legalcopilot/legal_copilot/LegalCopilotApplication.java
@@ -2,8 +2,10 @@ package com.legalcopilot.legal_copilot;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableAsync;
 
 @SpringBootApplication
+@EnableAsync
 public class LegalCopilotApplication {
 
 	public static void main(String[] args) {
